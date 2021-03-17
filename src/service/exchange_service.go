4617290c671bd1fe93189d6985461e4af9a61210
bejitono <stdemicheli@protonmail.com>
2021-03-18 03:37:34 +0700

    Implements currency validation.

--- src/service/exchange_service.go
+++ src/service/exchange_service.go
@@ -1,10 +1,16 @@
 package service
 
 import (
+	"strings"
+
 	exchange "github.com/bejitono/ln-exchange-api/src/domain"
 	db "github.com/bejitono/ln-exchange-api/src/repository/db"
 	rest "github.com/bejitono/ln-exchange-api/src/repository/rest"
 	"github.com/stdemicheli/bookstore_oauth-api/src/utils/errors"
+)
+
+const (
+	lnCurrency = "lnx"
 )
 
 type Service interface {
@@ -26,15 +32,19 @@ func NewService(restRepo rest.RestExchangeRepository, dbRepo db.DbRepository) Se
 }
 
 func (s *service) GetExchangeById(id int64) (*exchange.Exchange, *errors.RestErr) {
-	return nil, nil
+	return nil, errors.NewNotFoundError("not implemented")
 }
 
 func (s *service) GetExchanges() ([]exchange.Exchange, *errors.RestErr) {
-	return nil, nil
+	return nil, errors.NewNotFoundError("not implemented")
 }
 
 func (s *service) Withdraw(req exchange.WithdrawalRequest) *errors.RestErr {
 	// TODO: get exchange from exchange id
+	if strings.ToLower(req.Currency) != lnCurrency {
+		return errors.NewBadRequestError("Only lightning (lnx) is currently supported")
+	}
+
 	if err := s.restRepository.Withdraw(req); err != nil {
 		return errors.NewNotFoundError(err.Error)
 	}
